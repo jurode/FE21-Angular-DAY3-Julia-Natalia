3b516d262f2f644a116b95f2c657282a4c001d4d
Natalia <natalia_bochenek@hotmail.com>
2021-03-24 12:02:43 +0100

    add routes for product-details landingpages

--- PetShopNatalia/PetShop/src/app/app-routing.module.ts
+++ PetShopNatalia/PetShop/src/app/app-routing.module.ts
@@ -1,10 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
 const routes: Routes = [
   {
     path: '', component: ProductListComponent
+  },
+  {
+    path: 'product-details/:productID', component: ProductDetailsComponent
   }
 ];
 
