0e2f988ab60b67ca447f614e3a239ee0e5876c25
Natalia <natalia_bochenek@hotmail.com>
2021-03-24 14:07:28 +0100

    add route with productID

--- PetShopNatalia/PetShop/src/app/product-details/product-details.component.ts
+++ PetShopNatalia/PetShop/src/app/product-details/product-details.component.ts
@@ -1,4 +1,10 @@
 import { Component, OnInit } from '@angular/core';
+// import ActivatedRoute to grab a parameter with paramMap method
+import { ActivatedRoute } from '@angular/router';
+
+// import the productsAll from products.ts
+import { productsAll } from '../products';
+
 
 @Component({
   selector: 'product-details',
@@ -7,9 +13,17 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ProductDetailsComponent implements OnInit {
 
-  constructor() { }
+  product: any;
+
+  constructor(private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+    this.route.paramMap.subscribe(parameters => { // we take the parameters from the Map object; the subscribe() function defines how to obtain values to be published --> we grab the values from route.paramMap and put them to "parameters"
+      this.product = productsAll[+parameters.get('productID')]; // [] -> inside of these we create a way to connect the item's index with the productId --> with the method .get('') we return a specified element --productId-- from a Map object
+    })
+    console.log(this.product)
+    console.log(productsAll)
+
   }
 
 }
