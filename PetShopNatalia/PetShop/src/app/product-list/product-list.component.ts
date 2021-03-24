069ecc50a1115a8939e1a0b36628a4106c38492f
Natalia <natalia_bochenek@hotmail.com>
2021-03-24 16:47:51 +0100

    adding addToCart function

--- PetShopNatalia/PetShop/src/app/product-list/product-list.component.ts
+++ PetShopNatalia/PetShop/src/app/product-list/product-list.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { productsAll } from '../products';
+import { ShoppingCartService } from '../shopping-cart.service';
 
 @Component({
   selector: 'product-list',
@@ -10,8 +11,13 @@ export class ProductListComponent implements OnInit {
 
   products = productsAll; // 'products' is a name of my parameter ; 'productsAll' is the name of my imported array from the file products.ts
 
-  constructor() {
+  constructor( private shoppingCartService: ShoppingCartService) {
     console.log(this.products); // check if works
+  }
+
+  addProductsToCart(product) {
+    window.alert(`You added a product to your cart`);
+    this.shoppingCartService.addProductsToCart(product);
   }
 
   ngOnInit(): void {
