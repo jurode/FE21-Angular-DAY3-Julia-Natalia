8130b9a4ac402086bf9285bfc6313b02b7e03f0c
Natalia <natalia_bochenek@hotmail.com>
2021-03-24 11:52:57 +0100

    add image links to file

--- PetShopNatalia/PetShop/src/app/products.ts
+++ PetShopNatalia/PetShop/src/app/products.ts
@@ -1,44 +1,50 @@
 export const productsAll = [
   {
-    animalSpeciec: 'dog',
+    animalSpecies: 'dog',
     productName: 'Select Gold with chicken',
     productType:'food',
     weight: '200',
-    price: 5.49
+    price: 5.49,
+    img: 'assets/img/karsten-winegeart-5PVXkqt2s9k-unsplash.jpg'
   },
   {
-    animalSpeciec: 'dog',
+    animalSpecies: 'dog',
     productName: 'Real Nature pure beef',
     productType: 'food',
     weight: '400',
-    price: 15.49
+    price: 15.49,
+    img: 'assets/img/pauline-loroy-U3aF7hgUSrk-unsplash.jpg'
   },
   {
-    animalSpeciec: 'dog',
+    animalSpecies: 'dog',
     productName: 'PREMIERE beef & chicken',
     productType:'food',
     weight: '900',
-    price: 10.49
+    price: 10.49,
+    img: 'assets/img/alvan-nee-T-0EW-SEbsE-unsplash.jpg'
   },
   {
-    animalSpeciec: 'dog',
+    animalSpecies: 'dog',
     productName: 'Naturally good',
     productType: 'sleeping space',
 //    weight: '900',
-    price: 25.49
+    price: 25.49,
+    img: 'assets/img/jamie-street-s9Tf1eBDFqw-unsplash.jpg'
   },
   {
-    animalSpeciec: 'dog',
+    animalSpecies: 'dog',
     productName: 'FIT+FUN',
     productType: 'collar',
 //    weight: '900',
-    price: 10.40
+    price: 10.40,
+    img: 'assets/img/matt-nelson-aI3EBLvcyu4-unsplash.jpg'
   },
   {
-    animalSpeciec: 'dog',
+    animalSpecies: 'dog',
     productName: 'ANIONE',
     productType: 'playing ball',
 //    weight: '900',
-    price: 4.99
+    price: 4.99,
+    img: 'assets/img/anna-dudkova-urs_y9NwFcc-unsplash.jpg'
   }
 ];
