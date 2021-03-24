0cf05a60353ec5c3b14e8169765477a4f94499fc
Natalia <natalia_bochenek@hotmail.com>
2021-03-24 14:09:52 +0100

    add comments

--- PetShopNatalia/PetShop/src/app/product-details/product-details.component.ts
+++ PetShopNatalia/PetShop/src/app/product-details/product-details.component.ts
@@ -20,9 +20,10 @@ export class ProductDetailsComponent implements OnInit {
   ngOnInit(): void {
     this.route.paramMap.subscribe(parameters => { // we take the parameters from the Map object; the subscribe() function defines how to obtain values to be published --> we grab the values from route.paramMap and put them to "parameters"
       this.product = productsAll[+parameters.get('productID')]; // [] -> inside of these we create a way to connect the item's index with the productId --> with the method .get('') we return a specified element --productId-- from a Map object
+      console.log(productsAll[+parameters.get('productID')]) // returns object of productsAll[index]
     })
-    console.log(this.product)
-    console.log(productsAll)
+    console.log(this.product) // returns object of productsAll[index]
+    console.log(productsAll) // returns an array with all objects 
 
   }
 
