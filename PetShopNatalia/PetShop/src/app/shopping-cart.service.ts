b77bb263d0e0ae15bac872406576064fd21e767f
Natalia <natalia_bochenek@hotmail.com>
2021-03-24 14:25:56 +0100

    add new service file incl. functions

---
+++ PetShopNatalia/PetShop/src/app/shopping-cart.service.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class ShoppingCartService {
+
+  productsInCart = []; // create a empty array where each product will be temporarily saved -- until checkout or cleared
+
+  constructor() { }
+
+  // add functions
+
+  // create function to add items to cart
+  addProductsToCart(product) {
+    this.productsInCart.push(product);
+  }
+
+  // create function to add products to the cart and to return each product with its quantity
+  getProducts() {
+    return this.productsInCart;
+  }
+
+  // create function to remove the items from the cart and return an empty cart
+  clearCart() {
+    this.productsInCart = [];
+    return this.productsInCart;
+  }
+}
