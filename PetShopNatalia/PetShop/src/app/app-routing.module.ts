ab79289c36216fc99ec8690cc012cf490b1d2b6f
Natalia <natalia_bochenek@hotmail.com>
2021-03-24 15:42:45 +0100

    add router link basket

--- PetShopNatalia/PetShop/src/app/app-routing.module.ts
+++ PetShopNatalia/PetShop/src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { BasketComponent } from './basket/basket.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
 import { ProductListComponent } from './product-list/product-list.component';
 
@@ -9,6 +10,9 @@ const routes: Routes = [
   },
   {
     path: 'product-details/:productID', component: ProductDetailsComponent
+  },
+  {
+    path: 'basket', component: BasketComponent
   }
 ];
 
