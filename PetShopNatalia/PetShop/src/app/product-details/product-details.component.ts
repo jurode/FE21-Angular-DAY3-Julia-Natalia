c525be271ff136bda00a57108be48a10cae9daac
Natalia <natalia_bochenek@hotmail.com>
2021-03-24 16:47:33 +0100

    add space

--- PetShopNatalia/PetShop/src/app/product-details/product-details.component.ts
+++ PetShopNatalia/PetShop/src/app/product-details/product-details.component.ts
@@ -24,6 +24,7 @@ export class ProductDetailsComponent implements OnInit {
     window.alert(`You added a product to your cart`);
     this.shoppingCartService.addProductsToCart(product);
   }
+
   ngOnInit(): void { // https://angular.io/api/router/ParamMap
     this.route.paramMap.subscribe(parameters => { // we take the parameters from the Map object; the subscribe() function defines how to obtain values to be published --> we grab the values from route.paramMap and put them to "parameters"
       this.product = productsAll[+parameters.get('productID')]; // [] -> inside of these we create a way to connect the item's index with the productId --> with the method .get('') we return a specified element --productId-- from a Map object
