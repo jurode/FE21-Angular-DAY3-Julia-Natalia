16e2ba8b6f01ef558c6d8fdad6b8347fd9f4876c
Natalia <natalia_bochenek@hotmail.com>
2021-03-24 11:16:54 +0100

    add objects to file

---
+++ PetShopNatalia/PetShop/src/app/products.ts
@@ -0,0 +1,44 @@
+export const productsAll = [
+  {
+    animalSpeciec: 'dog',
+    productName: 'Select Gold with chicken',
+    productType:'food',
+    weight: '200',
+    price: 5.49
+  },
+  {
+    animalSpeciec: 'dog',
+    productName: 'Real Nature pure beef',
+    productType: 'food',
+    weight: '400',
+    price: 15.49
+  },
+  {
+    animalSpeciec: 'dog',
+    productName: 'PREMIERE beef & chicken',
+    productType:'food',
+    weight: '900',
+    price: 10.49
+  },
+  {
+    animalSpeciec: 'dog',
+    productName: 'Naturally good',
+    productType: 'sleeping space',
+//    weight: '900',
+    price: 25.49
+  },
+  {
+    animalSpeciec: 'dog',
+    productName: 'FIT+FUN',
+    productType: 'collar',
+//    weight: '900',
+    price: 10.40
+  },
+  {
+    animalSpeciec: 'dog',
+    productName: 'ANIONE',
+    productType: 'playing ball',
+//    weight: '900',
+    price: 4.99
+  }
+]
